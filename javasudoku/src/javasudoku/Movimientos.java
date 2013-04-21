593f848ac9e16c2adfe5103881725da5eaf03730
nico <nico@nico-Inspiron-1545.(none)>
2013-04-21 13:14:56 -0300

    Estructura inicial

---
+++ javasudoku/src/javasudoku/Movimientos.java
@@ -0,0 +1,89 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javasudoku;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 
+ */
+public class Movimientos {
+    private int tipo;
+    private int fila;
+    private int columna;
+    private int valor;
+    private Date fechaHora;
+
+    /**
+     * @return the tipo
+     */
+    public int getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the fila
+     */
+    public int getFila() {
+        return fila;
+    }
+
+    /**
+     * @param fila the fila to set
+     */
+    public void setFila(int fila) {
+        this.fila = fila;
+    }
+
+    /**
+     * @return the columna
+     */
+    public int getColumna() {
+        return columna;
+    }
+
+    /**
+     * @param columna the columna to set
+     */
+    public void setColumna(int columna) {
+        this.columna = columna;
+    }
+
+    /**
+     * @return the valor
+     */
+    public int getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the fechaHora
+     */
+    public Date getFechaHora() {
+        return fechaHora;
+    }
+
+    /**
+     * @param fechaHora the fechaHora to set
+     */
+    public void setFechaHora(Date fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+}
--- javasudoku/src/javasudoku/Sudoku.java
+++ javasudoku/src/javasudoku/Sudoku.java
@@ -1,6 +1,82 @@
 package javasudoku;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * Clase estructura de Sudoku
+ */
 public class Sudoku {
-    private int[][] matriz;
-    
+    private int[][] matriz_solucion;
+    private int[][] matriz_inicial;
+    private int[][] matriz_actual;
+    private List<Movimientos> listadoMovimientos;
+    /**
+     * Constructor
+     */
+    public Sudoku()
+    {
+        this.matriz_solucion = new int[8][8]; 
+        this.matriz_inicial = new int[8][8]; 
+        this.matriz_actual = new int[8][8]; 
+        this.listadoMovimientos = new ArrayList<Movimientos>();
+
+    }
+
+    /**
+     * @return the matriz_solucion
+     */
+    public int[][] getMatriz_solucion() {
+        return matriz_solucion;
+    }
+
+    /**
+     * @param matriz_solucion the matriz_solucion to set
+     */
+    public void setMatriz_solucion(int[][] matriz_solucion) {
+        this.matriz_solucion = matriz_solucion;
+    }
+
+    /**
+     * @return the matriz_inicial
+     */
+    public int[][] getMatriz_inicial() {
+        return matriz_inicial;
+    }
+
+    /**
+     * @param matriz_inicial the matriz_inicial to set
+     */
+    public void setMatriz_inicial(int[][] matriz_inicial) {
+        this.matriz_inicial = matriz_inicial;
+    }
+
+    /**
+     * @return the matriz_actual
+     */
+    public int[][] getMatriz_actual() {
+        return matriz_actual;
+    }
+
+    /**
+     * @param matriz_actual the matriz_actual to set
+     */
+    public void setMatriz_actual(int[][] matriz_actual) {
+        this.matriz_actual = matriz_actual;
+    }
+
+    /**
+     * @return the listadoMovimientos
+     */
+    public List<Movimientos> getListadoMovimientos() {
+        return listadoMovimientos;
+    }
+
+    /**
+     * @param listadoMovimientos the listadoMovimientos to set
+     */
+    public void setListadoMovimientos(List<Movimientos> listadoMovimientos) {
+        this.listadoMovimientos = listadoMovimientos;
+    }
 }
