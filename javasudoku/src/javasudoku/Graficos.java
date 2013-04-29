53a11f2f7324953f2e891cadabd0cc321788e8fb
Guillermo Lopez Quintanilla <reynor77@gmail.com>
2013-04-28 22:24:38 -0400

    agrega metodos para imprimir por pantalla el sodoku

--- javasudoku/src/javasudoku/Graficos.java
+++ javasudoku/src/javasudoku/Graficos.java
@@ -10,4 +10,44 @@ package javasudoku;
  */
 public class Graficos {
     
+    
+    /*
+     * Imprime sodoku
+     * recibe la matriz  y la muestra por pantalla.
+     */
+    public void ImprimeSodoku(int omatriz[][]){
+         try{            
+              for (int i = 0; i < omatriz.length; i++) {
+                  ImprimeSeparadores(i, "-");
+                for (int j = 0; j < omatriz[i].length; j++) {
+                      ImprimeSeparadores(j, "|");
+                      System.out.print(" " + omatriz[i][j] + " ");
+              }
+                ImprimeSeparadores(9, "|");
+                System.out.println();
+            }
+              ImprimeSeparadores(9, "-");
+          }
+          catch(Exception e){
+                 System.out.println("ERROR al imprimir Sodoku" + e.getStackTrace());
+          }
+    }
+    
+    /*
+     * Imprime el tipo de separador.
+     */
+    private void ImprimeSeparadores(int ij,String pSeparador ){
+        String separador = ((pSeparador.equals("|")? "|":"-------------------------------"));
+        switch(ij){
+            case 0:
+            case 3:
+            case 6:
+            case 9:
+                if(separador.equals("|"))
+                    System.out.print(separador);
+                else
+                     System.out.println(separador);
+            break;
+        }
+    }
 }
